9ecdf4222232e79cb57f57aa2e386f7c289ba8e8
Kabiru Eweje <ewejeabdulkabir@gmail.com>
2024-02-08 19:24:14 +0100

    script dynamic

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    num = len(sys.argv)
+    if num > 1:
+        if num == 2:
+            print("{:d} argument:".format(num -1))
+            # print("{:d}: {:s}".format(num -1, sys.argv))
+        else:
+            print("{:d} arguments:".format(num - 1))
+            # print("{:d}: {:s}".format(num - 1, sys.argv))
+        for i in range(1, num):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+    else:
+        print("{} arguments.".format(num - 1))
