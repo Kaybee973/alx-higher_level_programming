4a07ec4ee2d30054762f383245d7709d8007ab14
Kabiru Eweje <ewejeabdulkabir@gmail.com>
2024-02-08 17:32:18 +0100

    lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for alpha in range(122, 96, -1):
-    print("{:c}".format(alpha) if alpha % 2 ==0 else  "{}".format(chr(alpha - 32)))
+    print("{:c}".format(alpha) if alpha % 2 ==0 else  "{}".format(chr(alpha - 32)), end="")
