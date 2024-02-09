ac0c2394c8072119a3af8d1facc9c40047039e49
Kabiru Eweje <ewejeabdulkabir@gmail.com>
2024-02-09 11:50:20 +0100

    infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    import sys
-    num = len(sys.argv)
+def infinite_add(argv):
+    num = len(argv)
     if num > 1:
         i = 1
         a = 0
         while i < (num):
-            a += int(sys.argv[i])
+            a += int(argv[i])
             i += 1
         print("{:d}".format(a))
     else:
         print("{:d}".format(num - 1))
+if __name__ == '__main__':
+    import sys
+    infinite_add(sys.argv)
