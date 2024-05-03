3c327714c127036fa2e7455da40dd593f25f556a
Kabiru Eweje <ewejeabdulkabir@gmail.com>
2024-05-03 16:44:48 +0100

    debugging

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for alpha in range(122, 96, -1):
-    print("{:c}".format(alpha) if alpha % 2 ==0 else  "{}".format(chr(alpha - 32)), end="")
+
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
